4db90ee816a1bc373c710d7457b808ff1ad914ea
dMitsopoulou <190157454@aston.ac.uk>
2023-03-27 00:19:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //Node<String> root = new Node<>("root");
+        var random_execution = new RandomSearch();
+
+
+
+        //System.out.println("");
+    }
+
+
+    //evolutionary alg method
+    //initialise population with random candidate solutions
+    //Evaluate each candidate with cost function
+    //repeat until termination cond is satisfied
+    //select parents
+    //recombine pairs of parents
+    //mutate the resulting offspring
+    //evaluate new candidates
+    //select individuals for next generation
+    //End
+    //END
+}
---
+++ src/RandomSearch.java
@@ -0,0 +1,4 @@
+public class RandomSearch {
+
+    //take elements from v1 of salesman problem
+}
